bb0f858f9fccb28fd5aac99c7d278b9e09039b5c
venugopal.thippaneni <venugopal.thippaneni@medicaldirector.com>
2021-12-17 22:59:16 +0530

    enable swagger for all environments

--- src/Locator.Api/Startup.cs
+++ src/Locator.Api/Startup.cs
@@ -42,10 +42,10 @@ namespace Locator.Api
         {
             if (env.IsDevelopment())
             {
-                app.UseDeveloperExceptionPage();
-                app.UseSwagger();
-                app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "Locator.Api v1"));
+                app.UseDeveloperExceptionPage();                
             }
+            app.UseSwagger();
+            app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "Locator.Api v1"));
 
             app.UseRouting();
 
